1434879142992dd7cc809a70cbb6785635b53ff5
_ <_@lag5.com>
2023-12-18 19:03:42 +0800

    update xray geo data shell script

--- xray_geo_update/update_xray_geodata.sh
+++ xray_geo_update/update_xray_geodata.sh
@@ -8,10 +8,10 @@ GEOIP_CHECKSUM_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/dow
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VERSION/geosite.dat"
 GEOSITE_CHECKSUM_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/download/$LATEST_VERSION/geoip.dat.sha256sum"
 
-curl -m 5 -L -R -H "Cache-Control: no-cache" -o /tmp/geoip.dat.sha256sum $GEOIP_CHECKSUM_URL
+curl -m 15 -L -R -H "Cache-Control: no-cache" -o /tmp/geoip.dat.sha256sum $GEOIP_CHECKSUM_URL
 if [[ $? == 0 ]]
 then
-  curl -m 5 -L -R -H "Cache-Control: no-cache" -o /tmp/geoip.dat $GEOIP_URL
+  curl -m 15 -L -R -H "Cache-Control: no-cache" -o /tmp/geoip.dat $GEOIP_URL
   cd /tmp
   /usr/bin/sha256sum -c geoip.dat.sha256sum
   if [[ $? == 0 ]]
@@ -20,10 +20,10 @@ then
   fi
 fi
 
-curl -m 5 -L -R -H "Cache-Control: no-cache" -o /tmp/geosite.dat.sha256sum $GEOSITE_CHECKSUM_URL
+curl -m 15 -L -R -H "Cache-Control: no-cache" -o /tmp/geosite.dat.sha256sum $GEOSITE_CHECKSUM_URL
 if [[ $? == 0 ]]
 then
-  curl -m 5 -L -R -H "Cache-Control: no-cache" -o /tmp/geosite.dat $GEOSITE_URL
+  curl -m 15 -L -R -H "Cache-Control: no-cache" -o /tmp/geosite.dat $GEOSITE_URL
   cd /tmp
   /usr/bin/sha256sum -c geosite.dat.sha256sum
   if [[ $? == 0 ]]
